b928e93c3923cfc8f86fe6a7de340aed0eb2dfbe
Chia Xiong <chiaxiong19@gmail.com>
2024-07-15 15:55:52 -0400

    app.py flask update

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 import pickle
 import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 app = Flask(__name__)
 
@@ -10,9 +12,34 @@ with open('nearest_neighbor_model.pkl', 'rb') as file:
 
 @app.route('/api/predict', methods=['POST'])
 def predict():
+    # Get the request data
+    data = request.get_json(force=True)
 
-    return jsonify()
+    # Ensure the data is a list (even if it's just one dictionary)
+    if isinstance(data, dict):
+        data = [data]
+
+    # Define categorical features for one-hot encoding
+    categorical_features = ['name', 'type', 'price']
+
+    # Apply one-hot encoding to categorical features
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('onehot', OneHotEncoder(), categorical_features)
+        ],
+        remainder='passthrough'
+    )
+
+    # Make a prediction
+    prediction = model.predict(pd.DataFrame(data))
+    print("--DEBUG-- ",prediction.toList())
+
+    # Return the prediction
+    return jsonify(prediction.tolist())
 
 if __name__ == '__main__':
+    # app.debug = True
     app.run(port=5000)
+
+# curl -X POST http://localhost:5000/api/predict -H "Content-Type: application/json" -d '{"id": "2", "name": "Polo","type": "top","price": "11"}'
 
