1ccb4e6dbb46c4e998f8eb7e8074f001413df38d
Chia Xiong <chiaxiong19@gmail.com>
2024-07-16 04:36:20 -0400

    added recommended

--- flask/create_model.py
+++ flask/create_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+import json
 from dotenv import load_dotenv
 from pymongo import MongoClient
 import pandas as pd
@@ -20,20 +21,40 @@ from flask import Flask, request, jsonify
 # clothes = collection.find({})
 # df = pd.DataFrame(list(clothes))
 
-# Load the dataset
-df = pd.read_json('test.json')
+with open('test.json', 'rb') as file:
+    test = json.load(file)
 
-y = df['id']
-X = df[['type']]
+with open('database.json', 'rb') as file:
+    database = json.load(file)    
+
+recommend = {item['type']for item in test}
+result = []
+for item in database:
+    item_type = item['type']
+
+    if item_type in recommend:
+        recommend_value =1
+    else:
+        recommend_value =0
+    result.append({"id":item['id'],"name":item['name'],"type":item['type'],"recommended": recommend_value})
+
+
+# Load the dataset
+df = pd.DataFrame(result)
+
+# df = df.groupby(['id','name','type','price']).size().reset_index(name='purchase')
+
+y = df['recommended']
+X = df[['id','type']]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) #Note: test = selected for traning, test = not selected used for testing how accurate it works
 
 # Initialize the KNN classifier
-k=5 # Prediction based on k nearest neighbors
+k=min(5, len(X_train)) # Prediction based on k nearest neighbors
 knn = KNeighborsClassifier(n_neighbors=k)
 
 # Define categorical features for one-hot encoding
-categorical_features = ['type']
+categorical_features = ['id','type']
 
 # Apply one-hot encoding to categorical features
 preprocessor = ColumnTransformer(
@@ -58,6 +79,16 @@ y_pred = knn.predict(X_test_encoded)
 # Calculate the accuracy of the model
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
+
+result = df.to_json(orient="records")
+
+# # Call function generate clothes and dump info to json
+# json_object = json.dumps(result, indent=4)
+
+# Save data to json file
+with open("../react/recommended.json", "w") as file:
+    file.write(result)
+    print ("Done")
 
 # Save preprocessor to pipeline
 with open('preprocessor.pkl', "wb") as preprocessor_file:
