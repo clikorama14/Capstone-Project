985b507325f1646d555609b2a4ed85d2e7034b64
Chris Likorama <clikorama1@gmail.com>
2024-07-16 09:53:56 -0400

    styling commit

--- react/src/components/Recommended.jsx
+++ react/src/components/Recommended.jsx
@@ -1,16 +1,18 @@
 import React, { useState, useEffect } from "react";
 import recommended from '../recommended.json'
 import Product from './Product.jsx'
+import './Recommended.css';
 
 const RecommendedData = () => {
-  return (
-    <div className="recommended-container">
-      {
-        recommended.map((product) => (product.recommended === 1 &&
-          <Product key={ product.id} data={product} />
-        ))}
-    </div>
-  );
+    return (
+        
+        <div className="card-container">
+                {
+                    recommended.map((product) => (product.recommended === 1 &&
+                        <Product key={product.id} data={product} />
+                    ))}
+        </div>
+    );
 };
 
 export default RecommendedData;
